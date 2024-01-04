6e4dabb799574fbc20383cd83718a98f432c87fe
Sleepy_Miner <90234481+SleepyMiner@users.noreply.github.com>
2024-01-04 23:33:43 +0530

    added Peak Index in a Mountain Array

---
+++ 852. Peak Index in a Mountain Array/main.cpp
@@ -0,0 +1,22 @@
+#include<std/bitsc++.h>
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l = 0, r = arr.size() - 1;
+        int mid = l + (r - l)/2;
+        while(l < r)
+        {
+            if(arr[mid] < arr[mid+1])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid;
+            }
+            mid = l + (r - l)/2;
+        }
+        return l;
+    }
+};
